92f09a24085fa826c64d5c3b785f94498c09d3a2
PrynsTag <princevelasco16@gmail.com>
2021-05-09 21:28:26 +0800

    added button functionality

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        let imageArr = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+        
+        let RandomNumber = Int(arc4random_uniform(UInt32(imageArr.count)))
+        let image = UIImage.init(named: "\(imageArr[RandomNumber])")
+        
+        diceImageViewOne.image = image
+        diceImageViewTwo.image = image
+    }
+    
 }
+
 
